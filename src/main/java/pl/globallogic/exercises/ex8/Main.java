672ab8c5cf58102bde4ad3187c274ea6b7446d12
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-04-22 19:50:23 +0200

    feat: Exercise no.8

---
+++ src/main/java/pl/globallogic/exercises/ex8/Main.java
@@ -0,0 +1,26 @@
+package pl.globallogic.exercises.ex8;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+
+        System.out.println(isTeen(9));
+        System.out.println(isTeen(13));
+    }
+
+    public static boolean hasTeen(int a, int b, int c) {
+        if ((12 < a && a < 20) || (12 < b && b < 20) || (12 < c && c < 20)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen (int a) {
+        if (12 < a && a < 20) {
+            return true;
+        }
+        return false;
+    }
+}
