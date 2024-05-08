9a0f20aba31dd4a4cfc59ab046f2f3a5b802624a
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-05-08 19:37:47 +0200

    feat: Exercise no.34 & no.35

---
+++ src/main/java/pl/globallogic/exercises/ex35/Cylinder.java
@@ -0,0 +1,18 @@
+package pl.globallogic.exercises.ex35;
+
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
