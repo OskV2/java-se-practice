98bb87d789b02ea6552327bd024dc5a38b4f7dd9
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-05-08 19:02:38 +0200

    feat: Exercise no.33

---
+++ src/main/java/pl/globallogic/exercises/ex33/Calculator.java
@@ -0,0 +1,15 @@
+package pl.globallogic.exercises.ex33;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
