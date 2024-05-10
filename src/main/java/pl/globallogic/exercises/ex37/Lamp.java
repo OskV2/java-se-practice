426237c268dd89e0409e0d6624aa427379ae86ef
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-05-10 19:34:23 +0200

    feat: Exercise no.36 & no.37

---
+++ src/main/java/pl/globallogic/exercises/ex37/Lamp.java
@@ -0,0 +1,29 @@
+package pl.globallogic.exercises.ex37;
+
+public class Lamp {
+    private String style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public void turnOn() {
+        System.out.println("The lamp has been turned on");
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+}
